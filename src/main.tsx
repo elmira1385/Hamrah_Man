b16d2df3373279b24e61d49c75007b3ec4d80247
elmira1385 <elmirashokrian64@gmail.com>
2025-11-27 20:50:21 +0330

    v1

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Login from "./pages/Login";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>,
+  },
+ 
+]);
+
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+      <RouterProvider router={router} />
+  </StrictMode>
+);
+ 
